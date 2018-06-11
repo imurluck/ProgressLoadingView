b013e963cd5f452aa1c350e227c88fb85582655a
imurluck <1289042324@qq.com>
2018-06-11 22:52:43 +0800

    修改FailedView并编写READ.ME

--- app/src/main/java/com/example/zzx/progressloadingview/MainActivity.java
+++ app/src/main/java/com/example/zzx/progressloadingview/MainActivity.java
@@ -7,18 +7,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import com.example.library.ProgressLoadingView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    Button startAnimationBtn;
-    Button successBtn;
-    Button failedBtn;
-    Button addBtn;
-    ProgressLoadingView progressLoadingView;
+    private Button mStartAnimationBtn;
+    private Button mSuccessBtn;
+    private Button mFailedBtn;
+    private Button mAddBtn;
+
+    private ProgressLoadingView mBlueArcView;
+    private ProgressLoadingView mBlueWaterView;
+    private ProgressLoadingView mGreenArcView;
+    private ProgressLoadingView mGreenWaterView;
+
+    private SeekBar mSeekBar;
+
+    private List<ProgressLoadingView> mList;
     LinearLayout rootLayout;
 
 
@@ -26,57 +39,107 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /**successTickView = (SuccessTickView) findViewById(R.id.success_tick);
-         startAnimationBtn = (Button) findViewById(R.id.animate_btn);
-         startAnimationBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-        successTickView.startTickAnim();
-        }
-        });*/
+        mList = new ArrayList<>();
+        initViews();
+    }
+
+    private void initViews() {
         rootLayout = (LinearLayout) findViewById(R.id.root_layout);
-        progressLoadingView = (ProgressLoadingView) findViewById(R.id.loading_view);
-        startAnimationBtn = (Button) findViewById(R.id.start_btn);
-        failedBtn = (Button) findViewById(R.id.failed_btn);
-        successBtn = (Button) findViewById(R.id.success_btn);
-        addBtn = (Button) findViewById(R.id.add_btn);
-        startAnimationBtn.setOnClickListener(new View.OnClickListener() {
+        mSeekBar = (SeekBar) findViewById(R.id.seek_bar);
+        mBlueArcView = (ProgressLoadingView) findViewById(R.id.loading_view_blue_arc);
+        mBlueWaterView = (ProgressLoadingView) findViewById(R.id.loading_view_blue_water);
+        mGreenArcView = (ProgressLoadingView) findViewById(R.id.loading_view_green_arc);
+        mGreenWaterView = (ProgressLoadingView) findViewById(R.id.loading_view_green_water);
+        mList.add(mBlueArcView);
+        mList.add(mBlueWaterView);
+        mList.add(mGreenArcView);
+        mList.add(mGreenWaterView);
+        mStartAnimationBtn = (Button) findViewById(R.id.start_btn);
+        mFailedBtn = (Button) findViewById(R.id.failed_btn);
+        mSuccessBtn = (Button) findViewById(R.id.success_btn);
+        mAddBtn = (Button) findViewById(R.id.add_btn);
+        mStartAnimationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e(TAG, "onClick: " + Thread.currentThread());
-                progressLoadingView.startAnimation();
+                startAnimation();
             }
         });
-        successBtn.setOnClickListener(new View.OnClickListener() {
+        mSuccessBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //ProgressLoadingView.startAnimation(2);
-                progressLoadingView.setResult(ProgressLoadingView.RESULT_SUCCESS);
+                successAnimation();
             }
         });
-        failedBtn.setOnClickListener(new View.OnClickListener() {
+        mFailedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressLoadingView.setResult(ProgressLoadingView.RESULT_FAILED);
+                failAnimation();
             }
         });
 
-
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        mAddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final ProgressLoadingView view = new ProgressLoadingView.Builder().with(MainActivity.this)
                         .setColor(Color.BLUE)
                         .build();
+                mList.add(view);
                 rootLayout.addView(view, 0, new LinearLayout.LayoutParams(200, 200));
+                //请在view.post中调用{@link ProgressLoadingView#startAnimation()},因为有些初始化的计算
+                // 会在ProgressLoadingView加载完成之后
+                //please invoke {@link ProgressLoadingView#startAnimation()} in view.post, because of
+                //some calculation after ProgressLoadingView loaded.
                 view.post(new Runnable() {
                     @Override
                     public void run() {
-                        view.startAnimation();;
+                        view.startAnimation();
                     }
                 });
             }
         });
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setAnimationProgress(progress);
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    /**
+     *
+     * @param progress
+     */
+    private void setAnimationProgress(int progress) {
+        for (ProgressLoadingView view : mList) {
+            view.setProgress(progress);
+        }
+    }
+
+    private void successAnimation() {
+        for (ProgressLoadingView view : mList) {
+            view.setResult(ProgressLoadingView.RESULT_SUCCESS);
+        }
+    }
+
+    private void failAnimation() {
+        for (ProgressLoadingView view : mList) {
+            view.setResult(ProgressLoadingView.RESULT_FAILED);
+        }
+    }
+
+    private void startAnimation() {
+        for (ProgressLoadingView view : mList) {
+            view.startAnimation();
+        }
     }
 }
